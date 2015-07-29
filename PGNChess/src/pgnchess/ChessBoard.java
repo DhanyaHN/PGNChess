f882172ce1ff5b8ed784ed41f7aa26da4b10bb83
YashuAmaresh <yashaswini.amaresh@gmail.com>
2015-07-29 12:49:10 +0530

    Update ChessBoard.java

--- PGNChess/src/pgnchess/ChessBoard.java
+++ PGNChess/src/pgnchess/ChessBoard.java
@@ -18,8 +18,39 @@ public class ChessBoard {
     }
 
     private void initialize() {
+    	initializeFirstRow();
+    	for (int i = 2; i <= 7; ++i) {
+    		board[2][i] = new ChessPiece('W', 'S' );
+    		board[7][i] = new ChessPiece('B', 'S' );
+    	}
+    	for (int i = 3; i <= 6; ++i) {
+    		for (int j = 1; j <= 8; ++j) {
+    			board[i][j] = null;
+    		}
+    	}
         // initialize the board
     }
+    
+    private void initializeFirstRow() {
+    	board[1][1] = new ChessPiece('W', 'R');
+    	board[1][8] = new ChessPiece('W', 'R');
+    	board[1][2] = new ChessPiece('W', 'N');
+    	board[1][7] = new ChessPiece('W', 'N');
+    	board[1][3] = new ChessPiece('W', 'B');
+    	board[1][6] = new ChessPiece('W', 'B');
+    	board[1][4] = new ChessPiece('W', 'Q');
+    	board[1][5] = new ChessPiece('W', 'K');
+    	
+    	board[8][1] = new ChessPiece('B', 'R');
+    	board[8][8] = new ChessPiece('B', 'R');
+    	board[8][2] = new ChessPiece('B', 'N');
+    	board[8][7] = new ChessPiece('B', 'N');
+    	board[8][3] = new ChessPiece('B', 'B');
+    	board[8][6] = new ChessPiece('B', 'B');
+    	board[8][4] = new ChessPiece('B', 'K');
+    	board[8][5] = new ChessPiece('B', 'Q');
+    }
+    
     private void update() {
        // update the board 
     }
