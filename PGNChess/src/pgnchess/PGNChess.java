fc19fd6b5f06c9dbec05bace7b326659b56293d4
DhanyaHN <dhanyabharadwaj03@gmail.com>
2015-07-29 12:42:48 +0530

    Input file reading from input.txt

--- PGNChess/src/pgnchess/PGNChess.java
+++ PGNChess/src/pgnchess/PGNChess.java
@@ -5,6 +5,12 @@
  */
 package pgnchess;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author test
@@ -16,6 +22,26 @@ public class PGNChess {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ChessBoard board = new ChessBoard();
+        readInput("C:\\Users\\test\\Documents\\NetBeansProjects\\PGNChess\\PGNChess\\src\\pgnchess\\input");
+    }
+
+    private static void readInput(String path) {
+        File file = new File(path);
+        try {
+            Scanner in = new Scanner(file);
+            while (in.hasNextLine()) {
+                    String[] rec = in.nextLine().split(" ");
+                    System.out.println(rec[0] + "   " + rec[1]);
+                    board.update(rec[0], 'W');
+                    board.update(rec[1], 'B');
+
+                }
+                in.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(PGNChess.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
