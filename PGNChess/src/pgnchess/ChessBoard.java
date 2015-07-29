227c26e0ed9906c88d3811d417510cf647e94a80
RamprasadMurugesan <m.ramprasad.cbe@gmail.com>
2015-07-29 14:34:53 +0530

    Position class added

--- PGNChess/src/pgnchess/ChessBoard.java
+++ PGNChess/src/pgnchess/ChessBoard.java
@@ -1,27 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pgnchess;
 
 /**
  *
- * @author test
+ * @author Test
  */
 public class ChessBoard {
+    
     ChessPiece[][] board;
     
     public ChessBoard() {
-        board = new ChessPiece[9][9];
+        board = new ChessPiece[8][8];
         initialize();
     }
 
     private void initialize() {
     	initializeFirstRow();
     	for (int i = 2; i <= 7; ++i) {
-    		board[2][i] = new ChessPiece('W', 'P', 2, i);
-    		board[7][i] = new ChessPiece('B', 'P', 7, i);
+    		board[2][i] = new ChessPiece('W', 'P');
+    		board[7][i] = new ChessPiece('B', 'P');
     	}
     	for (int i = 3; i <= 6; ++i) {
     		for (int j = 1; j <= 8; ++j) {
@@ -32,46 +28,71 @@ public class ChessBoard {
     }
     
     private void initializeFirstRow() {
-    	board[1][1] = new ChessPiece('W', 'R', 1, 1);
-    	board[1][8] = new ChessPiece('W', 'R', 1, 8);
-    	board[1][2] = new ChessPiece('W', 'N', 1, 2);
-    	board[1][7] = new ChessPiece('W', 'N', 1, 7);
-    	board[1][3] = new ChessPiece('W', 'B', 1, 3);
-    	board[1][6] = new ChessPiece('W', 'B', 1, 6);
-    	board[1][4] = new ChessPiece('W', 'Q', 1, 4);
-    	board[1][5] = new ChessPiece('W', 'K', 1, 5);
+    	board[1][1] = new ChessPiece('W', 'R');
+    	board[1][8] = new ChessPiece('W', 'R');
+    	board[1][2] = new ChessPiece('W', 'N');
+    	board[1][7] = new ChessPiece('W', 'N');
+    	board[1][3] = new ChessPiece('W', 'B');
+    	board[1][6] = new ChessPiece('W', 'B');
+    	board[1][4] = new ChessPiece('W', 'Q');
+    	board[1][5] = new ChessPiece('W', 'K');
     	
-    	board[8][1] = new ChessPiece('B', 'R', 8, 1);
-    	board[8][8] = new ChessPiece('B', 'R', 8, 8);
-    	board[8][2] = new ChessPiece('B', 'N', 8, 2);
-    	board[8][7] = new ChessPiece('B', 'N', 8, 7);
-    	board[8][3] = new ChessPiece('B', 'B', 8, 3);
-    	board[8][6] = new ChessPiece('B', 'B', 8, 6);
-    	board[8][4] = new ChessPiece('B', 'K', 8, 4);
-    	board[8][5] = new ChessPiece('B', 'Q', 8, 5);
+    	board[8][1] = new ChessPiece('B', 'R');
+    	board[8][8] = new ChessPiece('B', 'R');
+    	board[8][2] = new ChessPiece('B', 'N');
+    	board[8][7] = new ChessPiece('B', 'N');
+    	board[8][3] = new ChessPiece('B', 'B');
+    	board[8][6] = new ChessPiece('B', 'B');
+    	board[8][4] = new ChessPiece('B', 'K');
+    	board[8][5] = new ChessPiece('B', 'Q');
     }
     
-    private void update() {
+    private void update(String move,char color ) {
+        
+        board[1][7].name = 'R';
+        
+        
+        
        // update the board 
     }
     private void display() {
         
     }
+    
 }
-
 class ChessPiece {
     char color;
     char name;
+    
+    
+    public ChessPiece(char color, char name) {
+        this.color = color;
+        this.name = name;
+        
+    }
+}
+class Position{
     int prev_x;
     int prev_y;
     int cur_x;
     int cur_y;
-    public ChessPiece(char color, char name, int prev_x, int prev_y) {
-        this.color = color;
-        this.name = name;
-        this.prev_x = prev_x;
-        this.prev_y = prev_y;
-        this.cur_x = cur_x;
-        this.cur_y = cur_y;
+    
+    public Position(int x, int y)
+    {
+        this.prev_x = x;
+        this.prev_y = y;
+        this.cur_x = x;
+        this.cur_y = y;
+         
     }
+    public void move(int next_x,int next_y)
+    {
+        this.prev_x = this.cur_x;
+        this.prev_y = this.cur_y;
+        this.cur_x = next_x;
+        this.cur_y = next_y;
+        
+    }
+    
+    
 }
